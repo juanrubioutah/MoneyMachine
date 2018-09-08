12f03d6f3ff3ca8a6aa0f2711189e98ebc0f9ff6
parkernilson <nilsopar@gmail.com>
2018-09-08 17:11:26 -0600

    Created the Machine class (main entry point)
    Machine.java is the main entry point for the program.

---
+++ src/Machine.java
@@ -0,0 +1,19 @@
+
+public class Machine {
+	
+	
+	public static void test(int parameter, int param2) {
+		
+	}
+	
+	public static void test(String parameter) {
+		
+	}
+	
+	public static void main(String args[]) {
+
+		
+
+	}
+
+}
