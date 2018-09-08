dc51a7ed6361b05b605fe7be9797c0bfe60ebe50
parkernilson <nilsopar@gmail.com>
2018-09-08 17:19:12 -0600

    removed garbage from Machine.java
    removed the test functions we were messing around with

--- src/Machine.java
+++ src/Machine.java
@@ -1,19 +1,8 @@
 
 public class Machine {
 	
-	
-	public static void test(int parameter, int param2) {
-		
-	}
-	
-	public static void test(String parameter) {
-		
-	}
-	
 	public static void main(String args[]) {
-
 		
-
 	}
 
 }
