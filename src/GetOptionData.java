8876188b0ca0e78a38dd771a5369883bb33c0191
parkernilson <nilsopar@gmail.com>
2018-09-08 17:52:28 -0600

    added jsoup to the build path

---
+++ src/GetOptionData.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Parker Nilson
+ * 
+ * The GetOptionData class contains an arraylist of OptionData objects
+ * and methods for obtaining that data from the internet.
+ *
+ */
+public class GetOptionData {
+	
+	public ArrayList<OptionData> optionData;
+
+	public void makeHTTPRequest(String url) {
+		
+	}
+}
--- src/TradeDeterminer.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class TradeDeterminer {
-	public TradeDeterminer(ArrayList<Option> options) {
-		
-	}
-}
