32279e8dcdb888f7d2e3add8d2dfbb17ff8581f2
parkernilson <nilsopar@gmail.com>
2018-09-09 02:49:33 -0600

    Created OptionDataRetriever
    this function fetches a JSON object of percentages about Options trade
    percentages from the internet then converts them into an ArrayList of
    OptionData objects and passes that to the constructor of the
    TradeDeterminer object in the main function

--- src/main/java/Money/MoneyMachine/Machine.java
+++ src/main/java/Money/MoneyMachine/Machine.java
@@ -1,8 +1,15 @@
 package Money.MoneyMachine;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Machine {
+	public static String customTableEquitiesURL = "https://cdn.optionseducation.org/rest/customtableitem.customtable.OICTradeAlertEquity?hash=8668d92358b2cb481080a7da3d75b9b8e7c4a2bee9768160655cc20e45afb105&format=json";
 	
-	public static void main(String args[]) {
-		
+	public static void main(String args[]) throws IOException {
+		OptionDataRetriever retriever = new OptionDataRetriever();
+		ArrayList<OptionData> options = retriever.retrieveOptionDataFrom(customTableEquitiesURL);
+		TradeDeterminer determiner = new TradeDeterminer(options);
 	}
 
 }
---
+++ src/main/java/Money/MoneyMachine/OptionDataRetriever.java
@@ -0,0 +1,82 @@
+package Money.MoneyMachine;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * 
+ * @author Parker Nilson
+ * 
+ * The GetOptionData class contains an arraylist of OptionData objects
+ * and methods for obtaining that data from the internet.
+ *
+ */
+public class OptionDataRetriever {
+	
+	private ArrayList<OptionData> optionData = new ArrayList<OptionData>();
+	private String jsonString;
+	
+	public ArrayList<OptionData> retrieveOptionDataFrom(String url) throws IOException{
+		retrieveJSON(url);
+		if(convertJsonToOptionData(jsonString)) {
+			return optionData;
+		}
+	
+		return null;
+	}
+	
+	public boolean convertJsonToOptionData(String jsonString) {
+		int curIndex = 0;
+
+		//run until there are no more occurences of "avg_total_calls", which means that there are no more options left in the jason array
+		while(jsonString.indexOf("\"avg_total_calls\":", curIndex + 1) > -1) {
+			//convert the string version of the json array of options objects into OptionData objects and insert them into the optionData ArrayList
+			curIndex = jsonString.indexOf("\"avg_total_calls\":", curIndex + 1); 
+			int valueIndex = curIndex + "\"avg_total_calls\":".length();
+			String strAvgTotalCalls = jsonString.substring(valueIndex, jsonString.indexOf(',', valueIndex));
+			int AvgTotalCalls = Integer.parseInt(strAvgTotalCalls);
+
+			curIndex = jsonString.indexOf("\"avg_total_puts\":", curIndex);
+			valueIndex = curIndex + "\"avg_total_puts\":".length();
+			String strAvgTotalPuts = jsonString.substring(valueIndex, jsonString.indexOf(',', valueIndex));
+			int AvgTotalPuts = Integer.parseInt(strAvgTotalPuts);
+			
+			curIndex = jsonString.indexOf("\"usymbol\":\"", curIndex);
+			valueIndex = curIndex + "\"usymbol\":\"".length();
+			String symbol = jsonString.substring(valueIndex, jsonString.indexOf("\"", valueIndex));
+			
+			int avgTotalOptions = AvgTotalCalls + AvgTotalPuts;
+			double percentCalls = (double)AvgTotalCalls / (double)avgTotalOptions;
+			double percentPuts = (double)AvgTotalPuts / (double)avgTotalOptions;
+			OptionData optionDataObject = new OptionData(symbol, percentCalls, percentPuts);
+			this.optionData.add(optionDataObject);
+		}
+		
+		//TODO: implement failure detection
+		return true;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param url
+	 * A string URL to collect JSON from.
+	 * 
+	 * @throws IOException
+	 */
+	public void retrieveJSON(String url) throws IOException {
+		URL u = new URL(url);
+		URLConnection con = u.openConnection();
+		InputStream in = con.getInputStream();
+		String encoding = con.getContentEncoding();
+		encoding = encoding == null ? "UTF-8" : encoding;
+		String body = IOUtils.toString(in, encoding);
+		//System.out.println(body);
+		
+		jsonString = body;
+	}
+}
